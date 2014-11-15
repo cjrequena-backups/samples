1023c88b161e5f46fa05e9f359e323b79dd7c321
cjrequena <carlosjose.requena@gmail.com>
2014-11-15 18:56:02 +0100

    Relection Utils Mod

--- sample-jpa-spring-4.x-jsf-2.x-primefaces-5.x/src/main/java/com/sample/architecture/commons/utils/ReflectionUtils.java
+++ sample-jpa-spring-4.x-jsf-2.x-primefaces-5.x/src/main/java/com/sample/architecture/commons/utils/ReflectionUtils.java
@@ -3,40 +3,19 @@ package com.sample.architecture.commons.utils;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class ReflectionUtils {
-	public static Object setAllSetters(Object ob, Object[] data) throws SQLException {
-		Class<? extends Object> cls = ob.getClass();
+	public static Object setAllSetters(Object target, Object[] data) throws Exception {
+		Class<? extends Object> targetClass = target.getClass();
 		for (int i = 0; i < data.length; i++) {
 
-			for (Field field : cls.getDeclaredFields()) {
-				for (Method method : cls.getMethods()) {
+			for (Field field : targetClass.getDeclaredFields()) {
+				for (Method method : targetClass.getMethods()) {
 					if ((method.getName().startsWith("set")) && (method.getName().length() == (field.getName().length() + 3))) {
 						if (method.getName().toLowerCase().endsWith(field.getName().toLowerCase())) {
 							try {
 								method.setAccessible(true);
-								if (field.getType().getSimpleName().toLowerCase().endsWith("integer")) {
-									method.invoke(ob, data[i]);
-								} else if (field.getType().getSimpleName().toLowerCase().endsWith("long")) {
-									method.invoke(ob, data[i]);
-								} else if (field.getType().getSimpleName().toLowerCase().endsWith("string")) {
-									method.invoke(ob, data[i]);
-								} else if (field.getType().getSimpleName().toLowerCase().endsWith("boolean")) {
-									method.invoke(ob, data[i]);
-								} else if (field.getType().getSimpleName().toLowerCase().endsWith("timestamp")) {
-									method.invoke(ob, data[i]);
-								} else if (field.getType().getSimpleName().toLowerCase().endsWith("date")) {
-									method.invoke(ob, data[i]);
-								} else if (field.getType().getSimpleName().toLowerCase().endsWith("double")) {
-									method.invoke(ob, data[i]);
-								} else if (field.getType().getSimpleName().toLowerCase().endsWith("float")) {
-									method.invoke(ob, data[i]);
-								} else if (field.getType().getSimpleName().toLowerCase().endsWith("time")) {
-									method.invoke(ob, data[i]);
-								} else {
-									method.invoke(ob, data[i]);
-								}
+								method.invoke(target, data[i]);
 								i++;
 							} catch (Exception e) {
 								System.err.println(e.getMessage());
@@ -46,38 +25,38 @@ public class ReflectionUtils {
 				}
 			}
 		}
-		return ob;
+		return target;
 	}
 
-	public static Object setAllSetters(Object ob, ResultSet rs) throws SQLException {
-		Class<? extends Object> cls = ob.getClass();
+	public static Object setAllSetters(Object target, ResultSet rs) throws Exception {
+		Class<? extends Object> targetClass = target.getClass();
 		while (rs.next()) {
-			for (Field field : cls.getDeclaredFields()) {
-				for (Method method : cls.getMethods()) {
+			for (Field field : targetClass.getDeclaredFields()) {
+				for (Method method : targetClass.getMethods()) {
 					if ((method.getName().startsWith("set")) && (method.getName().length() == (field.getName().length() + 3))) {
 						if (method.getName().toLowerCase().endsWith(field.getName().toLowerCase())) {
 							try {
 								method.setAccessible(true);
 								if (field.getType().getSimpleName().toLowerCase().endsWith("integer")) {
-									method.invoke(ob, rs.getInt(field.getName().toLowerCase()));
+									method.invoke(target, rs.getInt(field.getName().toLowerCase()));
 								} else if (field.getType().getSimpleName().toLowerCase().endsWith("long")) {
-									method.invoke(ob, rs.getLong(field.getName().toLowerCase()));
+									method.invoke(target, rs.getLong(field.getName().toLowerCase()));
 								} else if (field.getType().getSimpleName().toLowerCase().endsWith("string")) {
-									method.invoke(ob, rs.getString(field.getName().toLowerCase()));
+									method.invoke(target, rs.getString(field.getName().toLowerCase()));
 								} else if (field.getType().getSimpleName().toLowerCase().endsWith("boolean")) {
-									method.invoke(ob, rs.getBoolean(field.getName().toLowerCase()));
+									method.invoke(target, rs.getBoolean(field.getName().toLowerCase()));
 								} else if (field.getType().getSimpleName().toLowerCase().endsWith("timestamp")) {
-									method.invoke(ob, rs.getTimestamp(field.getName().toLowerCase()));
+									method.invoke(target, rs.getTimestamp(field.getName().toLowerCase()));
 								} else if (field.getType().getSimpleName().toLowerCase().endsWith("date")) {
-									method.invoke(ob, rs.getDate(field.getName().toLowerCase()));
+									method.invoke(target, rs.getDate(field.getName().toLowerCase()));
 								} else if (field.getType().getSimpleName().toLowerCase().endsWith("double")) {
-									method.invoke(ob, rs.getDouble(field.getName().toLowerCase()));
+									method.invoke(target, rs.getDouble(field.getName().toLowerCase()));
 								} else if (field.getType().getSimpleName().toLowerCase().endsWith("float")) {
-									method.invoke(ob, rs.getFloat(field.getName().toLowerCase()));
+									method.invoke(target, rs.getFloat(field.getName().toLowerCase()));
 								} else if (field.getType().getSimpleName().toLowerCase().endsWith("time")) {
-									method.invoke(ob, rs.getTime(field.getName().toLowerCase()));
+									method.invoke(target, rs.getTime(field.getName().toLowerCase()));
 								} else {
-									method.invoke(ob, rs.getObject(field.getName().toLowerCase()));
+									method.invoke(target, rs.getObject(field.getName().toLowerCase()));
 								}
 							} catch (Exception e) {
 								System.err.println(e.getMessage());
@@ -87,7 +66,30 @@ public class ReflectionUtils {
 				}
 			}
 		}
-		return ob;
+		return target;
 	}
 
+	public static Object setAllSetters(Object target, Object source) throws Exception {
+		Class<? extends Object> targetClass = target.getClass();
+		Class<? extends Object> sourceClass = source.getClass();
+
+		for (Method sourceMethod : sourceClass.getMethods()) {
+			String sourceMethodName = sourceMethod.getName();
+			if ((sourceMethodName.startsWith("get"))) {
+				for (Method targetMethod : targetClass.getMethods()) {
+					String targetMethodName = targetMethod.getName();
+					if ((targetMethodName.startsWith("set"))) {
+						if (sourceMethodName.substring(3, sourceMethodName.length()).equals(targetMethodName.substring(3, targetMethodName.length()))) {
+							targetMethod.invoke(target, sourceMethod.invoke(source));
+						}
+					}
+
+				}
+
+			}
+
+		}
+
+		return target;
+	}
 }
