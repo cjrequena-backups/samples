14720ca86ee73582c3531a5434eb580baa70b3a5
cjrequena <carlosjose.requena@gmail.com>
2014-12-14 20:17:00 +0100

    miscellaneous

--- sample-java-wrapper-service/src/main/java/com/sample/SampleWrapperService.java
+++ sample-java-wrapper-service/src/main/java/com/sample/SampleWrapperService.java
@@ -2,11 +2,10 @@ package com.sample;
 
 import org.apache.log4j.Logger;
 
-
 public class SampleWrapperService implements Runnable {
 
 	static Logger logger = Logger.getLogger(SampleWrapperService.class);
-	
+
 	/**
 	 * Le indica al metodo runService que necesita que detenga el servicio.
 	 */
@@ -29,7 +28,7 @@ public class SampleWrapperService implements Runnable {
 	@Override
 	public void run() {
 		this.m_stopping = false;
-		
+
 		/** El Grupo para los hilos de Trbajos */
 		ThreadGroup workGroup1 = new ThreadGroup("workGroup1");
 		ThreadGroup workGroup2 = new ThreadGroup("workGroup2");
@@ -40,7 +39,7 @@ public class SampleWrapperService implements Runnable {
 		final long threadCheckTimeout = ConfigurationManager.getLongProperty(ConfigurationManager.SERVICE_THREAD_CHECKTIMEOUT);
 
 		while (!this.m_stopping) {
-			
+
 			// Si algun hilo esta detenido lo volvemos a levantar, creando un nuevo session
 			for (int i = 0; i < threads2.length; i++) {
 				if (!m_stopping && (threads2[i] == null || !threads2[i].isAlive())) {
@@ -69,67 +68,66 @@ public class SampleWrapperService implements Runnable {
 				System.gc();
 			} catch (Exception ignore) {
 			}
-			
-			 // Interrumpimos el grupo de trbajo y la sessiones
-		    workGroup1.interrupt();
-		    workGroup2.interrupt();
-		    
-		    // El Proceso de eliminar los Threads.
-		    for (int i=0; i < threads1.length; i++) {
-		        if (threads1[i].isAlive()) {
-		            try {
-		            	if(logger.isDebugEnabled()) {
-		            		logger.debug("Esperando por " + threads1[i].getName());
-		            	}
-		            	//threads1[i].Stop();
-		                threads1[i].join();
-		                threads1[i] = null;
-		            } catch (InterruptedException ie) {
-		                try {
-		                    threads1[i].interrupt();
-		                }catch(Exception e) {
-		                    // ignoramos               
-		                }
-		            }
-		        }else {
-		        	if(logger.isDebugEnabled()) {
-		        		logger.debug("No Espera por " + threads1[i].getName());
-		        	}
-		        }
-		    }
-		    
-		    // El Proceso de eliminar los Threads.
-		    for (int i=0; i < threads2.length; i++) {
-		        if (threads2[i].isAlive()) {
-		            try {
-		            	if(logger.isDebugEnabled()) {
-		            		logger.debug("Esperando por " + threads2[i].getName());
-		            	}
-		            	//threads2[i].Stop();
-		                threads2[i].join();
-		                threads2[i] = null;
-		            } catch (InterruptedException ie) {
-		                try {
-		                    threads2[i].interrupt();
-		                }catch(Exception e) {
-		                    // ignoramos                   
-		                }
-		            }
-		        }else {
-		        	if(logger.isDebugEnabled()) {
-		        		logger.debug("No Espera por " + threads2[i].getName());
-		        	}
-		        }
-		    }
-		    
-		    try {
-		        workGroup1.destroy();
-		        workGroup2.destroy();
-		    }catch(Exception e){
-		    	logger.error("Error in destroy group - ignorado", e);
-		    }
 		}
 
+		// Interrumpimos el grupo de trbajo y la sessiones
+		workGroup1.interrupt();
+		workGroup2.interrupt();
+
+		// El Proceso de eliminar los Threads.
+		for (int i = 0; i < threads1.length; i++) {
+			if (threads1[i].isAlive()) {
+				try {
+					if (logger.isDebugEnabled()) {
+						logger.debug("Esperando por " + threads1[i].getName());
+					}
+					// threads1[i].Stop();
+					threads1[i].join();
+					threads1[i] = null;
+				} catch (InterruptedException ie) {
+					try {
+						threads1[i].interrupt();
+					} catch (Exception e) {
+						// ignoramos
+					}
+				}
+			} else {
+				if (logger.isDebugEnabled()) {
+					logger.debug("No Espera por " + threads1[i].getName());
+				}
+			}
+		}
+
+		// El Proceso de eliminar los Threads.
+		for (int i = 0; i < threads2.length; i++) {
+			if (threads2[i].isAlive()) {
+				try {
+					if (logger.isDebugEnabled()) {
+						logger.debug("Esperando por " + threads2[i].getName());
+					}
+					// threads2[i].Stop();
+					threads2[i].join();
+					threads2[i] = null;
+				} catch (InterruptedException ie) {
+					try {
+						threads2[i].interrupt();
+					} catch (Exception e) {
+						// ignoramos
+					}
+				}
+			} else {
+				if (logger.isDebugEnabled()) {
+					logger.debug("No Espera por " + threads2[i].getName());
+				}
+			}
+		}
+
+		try {
+			workGroup1.destroy();
+			workGroup2.destroy();
+		} catch (Exception e) {
+			logger.error("Error in destroy group - ignorado", e);
+		}
 	}
 
 	public void setAppArgs(String m_appArgs[]) {
